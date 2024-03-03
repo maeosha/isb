5f2178bf3fcbecbeb441c2466e102df569e34909
maeosha <mochkauha123@gmail.com>
2024-03-03 13:34:52 +0400

    collecting statistics and selecting the correct key

--- lab_1/task2.py
+++ lab_1/task2.py
@@ -8,3 +8,29 @@ def read_from_file() -> str:
             encrypted_text += i.replace('\n', '')
 
     return encrypted_text
+
+def get_stats(encrypted_text: str) -> tuple:
+    """Calculating the percentage of letters meeting and decoding the text"""
+    stats: dict = dict()
+    ave_stats: str = " оаинетслвпкурдябмьызюйгщэчжъфщхц"
+
+    for letter in encrypted_text:
+        if letter in stats:
+            stats[letter] += 1
+        else:
+            stats[letter] = 1
+
+    for stat in stats:
+        stats[stat] = stats[stat] / len(encrypted_text)
+
+    stats = dict(sorted(stats.items(), key=lambda x: x[1], reverse=True))
+    keys = list(stats.keys())
+    item = list(stats.items())
+    i = 0
+
+    for stat in ave_stats:
+        encrypted_text = encrypted_text.replace(keys[i], stat)
+        i += 1
+
+    return ave_stats, item
+
