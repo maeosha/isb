c064b802012fa9503a348a6b2d8f3d42a93a76ea
maeosha <mochkauha123@gmail.com>
2024-04-23 00:14:12 +0400

    random sequence generation in java

---
+++ lab_2/Generator/generator.java
@@ -0,0 +1,23 @@
+package java.util.Random;
+
+public class Main
+{
+    public static String GetRandomNumbers(int count) {
+        /*
+         * Generating a pseudorandom bit sequence
+         */
+
+        Random random = new Random();
+        String random_sequence = "";
+
+        for (int i = 0; i < count; i++) {
+            random_sequence += random.nextInt(2);
+        }
+        return random_sequence;
+    }
+
+	public static void main(String[] args) {
+		String random_sequence = GetRandomNumbers(128);
+		System.out.print(random_sequence);
+	}
+}
