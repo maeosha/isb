662cc282924adc36c96bf14ceb8c4a7b56a799ff
maeosha <mochkauha123@gmail.com>
2024-05-02 18:05:02 +0400

    serialization of a asymmetric key

--- lab_3/file_work.py
+++ lab_3/file_work.py
@@ -1,10 +1,33 @@
 import logging
 
+from cryptography.hazmat.primitives import serialization
+
 
 def serialize_symmetric_key(path_to_serialize_symmetric_key: str, key: bytes):
     try:
         with open(path_to_serialize_symmetric_key, 'wb') as key_file:
             key_file.write(key)
+    except OSError as error:
+        logging.warning(error)
+
+    except Exception as error:
+        logging.warning(error)
+
+def serialize_asymmetric_key(path_to_serialize_public_key: str, path_to_serialize_private_key: str, public_key, private_key):
+    try:
+        with open(path_to_serialize_public_key, 'wb') as key_file:
+            key_file.write(public_key.public_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PublicFormat.SubjectPublicKeyInfo)
+            )
+
+        with open(path_to_serialize_private_key, "wb") as key_file:
+            key_file.write(private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption())
+            )
+
     except OSError as error:
         logging.warning(error)
 
--- lab_3/secure_crypto_toolkit.py
+++ lab_3/secure_crypto_toolkit.py
@@ -1,7 +1,21 @@
 import os
 
-from file_work import serialize_symmetric_key
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+from file_work import serialize_symmetric_key, serialize_asymmetric_key
 
 def generation_keys(path_to_serialize_symmetric_key: str, path_to_serialize_public_key: str, path_to_serialize_privete_key: str):
     key: bytes = os.urandom(16)
     serialize_symmetric_key(path_to_serialize_symmetric_key, key)
+
+    keys = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+
+    private_key = keys
+    public_key = keys.public_key()
+    serialize_asymmetric_key(path_to_serialize_public_key,
+                             path_to_serialize_privete_key,
+                             public_key,
+                             private_key)
